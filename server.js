ab290428363807e2277cb69e596ad6c79a28c5ea
Jashanpreet Singh <jashankhaira52@gmail.com>
2024-10-11 20:08:26 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8080;
+
+// Set up the static files directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve the homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
